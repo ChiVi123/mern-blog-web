ff7e783c11e2c50f0e561a81a35451335ea6e8c5
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-22 21:58:17 +0700

    add middleware handle error

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import "dotenv-expand/config";
 import "dotenv/config";
-import express from "express";
+import express, { Response } from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.route";
 
@@ -13,9 +13,18 @@ mongoose
 
 const app = express();
 
+// config
 app.use(express.json());
 
+// routes
 app.use("/api/auth", authRoutes);
+
+// middleware
+app.use((err: Error, _req: any, res: Response, _next: any) => {
+    const statusCode = "statusCode" in err && typeof err.statusCode === "number" ? err.statusCode : 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({ success: false, statusCode, message });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
