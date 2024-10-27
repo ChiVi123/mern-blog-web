2fda3801346458b655566a8d7f25a363ab08eb26
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-27 12:22:03 +0700

    refactor(helper get axios error): check type axios error

--- user-client/src/shared/helper/get-axios-error.ts
+++ user-client/src/shared/helper/get-axios-error.ts
@@ -1,9 +1,17 @@
-import { AxiosError } from 'axios';
+import axios from 'axios';
 
-const errorDefault = {
+interface IErrorData {
+    success: false;
+    statusCode: number;
+    message: string;
+}
+
+const errorDefault: IErrorData = {
     success: false,
     statusCode: 500,
     message: 'Internal Server Error',
 };
 
-export const getAxiosError = (error: unknown) => (error instanceof AxiosError ? error.response?.data : errorDefault);
+export const getAxiosError = (error: unknown) => {
+    return axios.isAxiosError<IErrorData>(error) && error.response ? error.response.data : errorDefault;
+};
