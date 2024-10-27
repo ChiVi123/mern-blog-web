9e62548d8b69ff46c80f07d169f5ab8d4d875c87
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-27 20:19:06 +0700

    complete post detail page

--- user-client/src/view/post-detail/page.tsx
+++ user-client/src/view/post-detail/page.tsx
@@ -1,5 +1,59 @@
+import { Button, Spinner } from 'flowbite-react';
+import { Link, useLoaderData } from 'react-router-dom';
+import { CallToAction, PostCard } from '~components';
+import { IPostEntity } from '~modules/post';
+
 function PostDetailPage() {
-    return <div>PostDetail</div>;
+    const loader = useLoaderData() as { post: IPostEntity; recentPosts: IPostEntity[] } | undefined;
+
+    if (!loader)
+        return (
+            <div className='flex justify-center items-center min-h-screen'>
+                <Spinner size='xl' />
+            </div>
+        );
+
+    return (
+        <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+            <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+                {loader.post.title}
+            </h1>
+            <Link to={`/search?category=${loader.post.category}`} className='self-center mt-5'>
+                <Button color='gray' pill size='xs'>
+                    {loader.post.category}
+                </Button>
+            </Link>
+
+            <img
+                src={loader.post.image}
+                alt={loader.post.title}
+                className='mt-10 p-3 max-h-[600px] w-full object-cover'
+            />
+
+            <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+                <span>{new Date(loader.post.createdAt).toLocaleDateString()}</span>
+                <span className='italic'>{(loader.post.content.length / 1000).toFixed(0)} mins read</span>
+            </div>
+
+            <div
+                className='p-3 max-w-2xl mx-auto w-full post-content'
+                dangerouslySetInnerHTML={{ __html: loader.post.content }}
+            ></div>
+
+            <div className='max-w-4xl mx-auto w-full'>
+                <CallToAction />
+            </div>
+
+            {/* <CommentSection postId={loader.post.post._id} /> */}
+
+            <div className='flex flex-col justify-center items-center mb-5'>
+                <h1 className='text-xl mt-5'>Recent articles</h1>
+                <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+                    {loader.recentPosts && loader.recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default PostDetailPage;
--- user-client/src/view/post-detail/router.ts
+++ user-client/src/view/post-detail/router.ts
@@ -1,7 +1,24 @@
 import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
+import { postListRepo } from '~modules/post';
 
 export const postDetailRouter: RouteObject = {
     path: '/post/:slug',
     Component: lazy(() => import('./page')),
+    loader: async ({ params: { slug } }) => {
+        const resultBySlug = await postListRepo({ slug });
+        const resultRecent = await postListRepo({ limit: 3 });
+
+        if ('success' in resultBySlug) {
+            console.log(resultBySlug);
+            return;
+        }
+
+        if ('success' in resultRecent) {
+            console.log(resultRecent);
+            return;
+        }
+
+        return { post: resultBySlug.posts[0], recentPosts: resultRecent.posts };
+    },
 };
